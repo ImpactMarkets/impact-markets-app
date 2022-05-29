18681ad9b03bf81ddb9a8e35dc1cfc2f0b035ec9
Denis Drescher <denis.drescher@claviger.net>
2022-05-29 21:34:05 +0200

    Separate out tooltip component

--- components/text-field.tsx
+++ components/text-field.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { classNames } from '@/lib/classnames'
 import * as Tooltip from '@radix-ui/react-tooltip'
-import { InfoIcon } from './icons'
+import { InfoTooltip } from './info-tooltip'
 import { Label } from './label'
 
 export type TextFieldOwnProps = {
@@ -22,37 +22,7 @@ export const TextField = React.forwardRef<HTMLInputElement, TextFieldProps>(
         {label && (
           <Label htmlFor={id || name} className="block mb-2 text-s">
             {label}
-            {info && (
-              <Tooltip.Root delayDuration={0}>
-                <Tooltip.Trigger
-                  asChild
-                  onClick={(event) => {
-                    event.preventDefault()
-                  }}
-                  onMouseDown={(event) => {
-                    event.preventDefault()
-                  }}
-                >
-                  <span>
-                    {' '}
-                    <InfoIcon className="inline h-3" />
-                  </span>
-                </Tooltip.Trigger>
-                <Tooltip.Content
-                  side="bottom"
-                  sideOffset={4}
-                  className={classNames(
-                    'max-w-[260px] px-3 py-1.5 rounded shadow-lg bg-secondary-inverse text-secondary-inverse sm:max-w-sm'
-                  )}
-                >
-                  <p className="text-sm">{info}</p>
-                  <Tooltip.Arrow
-                    offset={22}
-                    className="fill-gray-800 dark:fill-gray-50"
-                  />
-                </Tooltip.Content>
-              </Tooltip.Root>
-            )}
+            {info && <InfoTooltip text={info} />}
           </Label>
         )}
         <input
