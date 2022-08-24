820563bd4bbcf145d1889fa8a822a46f01ae0bba
Dawn Drescher <dawn.drescher@impactmarkets.io>
2022-08-24 22:18:19 +0200

    Add transaction table for cert and profile

--- components/certificate/Transactions.tsx
+++ components/certificate/Transactions.tsx
@@ -1,14 +1,19 @@
-import { formatDistanceToNow } from 'date-fns'
 import * as React from 'react'
 
-import { InferQueryOutput, trpc } from '@/lib/trpc'
+import { trpc } from '@/lib/trpc'
 import { InferQueryPathAndInput } from '@/lib/trpc'
 import { TransactionState } from '@prisma/client'
 
+import { Author } from '../author'
+import { ButtonLink } from '../button-link'
+import { Date } from '../date'
+import { CancelDialog } from './CancelDialog'
+import { ConfirmDialog } from './ConfirmDialog'
+
 function getTransactionQueryPathAndInput(
   userId: string,
-  certificateId: number,
-  state: TransactionState
+  certificateId?: number,
+  state?: TransactionState
 ): InferQueryPathAndInput<'transaction.feed'> {
   return [
     'transaction.feed',
@@ -21,50 +26,109 @@ function getTransactionQueryPathAndInput(
 }
 
 export type TransactionsFormData = {
-  certificate: InferQueryOutput<'certificate.detail'>
-  user: InferQueryOutput<'user.profile'>
+  userId: string
+  certificateId?: number
 }
 
-export const Transactions = ({ certificate, user }: TransactionsFormData) => {
-  const utils = trpc.useContext()
+export const Transactions = ({
+  userId,
+  certificateId,
+}: TransactionsFormData) => {
+  const [isCancelDialogOpen, setIsCancelDialogOpen] = React.useState(false)
+  const [isConfirmDialogOpen, setIsConfirmDialogOpen] = React.useState(false)
+
   const transactionQueryPathAndInput = getTransactionQueryPathAndInput(
-    user.id,
-    certificate.id,
+    userId,
+    certificateId,
     'PENDING'
   )
   const transactionQuery = trpc.useQuery(transactionQueryPathAndInput)
 
-  if (transactionQuery.data) {
-    return (
-      <table className="w-full">
-        <thead>
-          <tr>
-            <th className="text-right pl-5">Created</th>
-            <th className="text-right pl-5">Size</th>
-            <th className="text-right pl-5">Cost</th>
-            <th className="text-right pl-5">Consume</th>
-          </tr>
-        </thead>
-        <tbody>
-          {transactionQuery.data.map((transaction) => (
-            <tr key={transaction.id}>
-              <td className="text-right pl-5">
-                <time dateTime={transaction.createdAt.toISOString()}>
-                  {formatDistanceToNow(transaction.createdAt)}
-                </time>{' '}
-                ago
-              </td>
-              <td className="text-right pl-5">{+transaction.size}%</td>
-              <td className="text-right pl-5">${+transaction.cost}</td>
-              <td className="text-right pl-5">{transaction.consume && '✅'}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
-  }
-
   if (transactionQuery.isError) {
     return <div>Error: {transactionQuery.error.message}</div>
   }
+
+  if (!transactionQuery.data?.length) {
+    return null
+  }
+
+  return (
+    <table className="w-full">
+      <thead>
+        <tr>
+          <th className="text-left pl-5">Created by</th>
+          <th className="text-left pl-5">At</th>
+          <th className="text-right pl-5">Size</th>
+          <th className="text-right pl-5">Cost</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactionQuery.data.map((transaction) => (
+          <tr key={transaction.id}>
+            <td className="pl-5">
+              <div className="pt-[5px]">
+                <Author author={transaction.buyingHolding.user} />
+              </div>
+            </td>
+            <td className="pl-5">
+              <Date date={transaction.createdAt} />
+            </td>
+            <td
+              className="text-right pl-5 underline underline-offset-1 decoration-dotted"
+              title={transaction.consume ? 'To be consumed' : 'To be owned'}
+            >
+              {+transaction.size * 100}%
+            </td>
+            <td className="text-right pl-5">${+transaction.cost}</td>
+            <td className="text-right px-2">
+              <ButtonLink
+                href="#"
+                onClick={() => {
+                  setIsCancelDialogOpen(true)
+                }}
+              >
+                <span className="block shrink-0">
+                  {/* Call it “Decline” if the user whose profile this is is also the seller;
+                    call it cancel if they’re the buyer. This ignores the case where an admin
+                    looks at the profile. No other user can see this section. */}
+                  {transaction.sellingHolding.user.id === userId
+                    ? 'Decline'
+                    : 'Cancel'}
+                </span>
+              </ButtonLink>
+              <CancelDialog
+                transactionId={transaction.id}
+                certificateId={certificateId}
+                isOpen={isCancelDialogOpen}
+                onClose={() => {
+                  setIsCancelDialogOpen(false)
+                }}
+              />
+              {transaction.sellingHolding.user.id === userId && (
+                <>
+                  <ButtonLink
+                    href="#"
+                    className="mx-1"
+                    onClick={() => {
+                      setIsCancelDialogOpen(true)
+                    }}
+                  >
+                    <span className="block shrink-0">Confirm</span>
+                  </ButtonLink>
+                  <ConfirmDialog
+                    transactionId={transaction.id}
+                    certificateId={certificateId}
+                    isOpen={isConfirmDialogOpen}
+                    onClose={() => {
+                      setIsConfirmDialogOpen(false)
+                    }}
+                  />
+                </>
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
