24e07d5e22d9de49f45dbbd50704e9e657e78f4c
Dawn Drescher <dawn.drescher@impactmarkets.io>
2022-09-03 22:29:29 +0200

    Create data migration to create the initial holdings and cuids

---
+++ prisma/migrations/20220825225711_create_initial_holdings/migration.sql
@@ -0,0 +1,55 @@
+insert into "Holding" ("certificateId", "userId", "type", "size", "cost") values
+  (2, 'cl3ahmzcn0123a4opvnc35uyg', 'OWNERSHIP', 1, 0),
+  (3, 'cl3ahq4wj0182a4opv1v3zcvs', 'OWNERSHIP', 1, 0),
+  (4, 'cl3aelpg70006rxopul24lqxm', 'OWNERSHIP', 1, 0),
+  (6, 'cl3ahq4wj0182a4opv1v3zcvs', 'OWNERSHIP', 1, 0),
+  (7, 'cl3w7u2uc1195stopujdvit9q', 'OWNERSHIP', 1, 0),
+  (8, 'cl3w7i9gn1111stop8guunoqc', 'OWNERSHIP', 1, 0),
+  (9, 'cl3xh0b540123rpopqgdw2gut', 'OWNERSHIP', 1, 0),
+ (10, 'cl3yhfeb80346jxopyc94e1v3', 'OWNERSHIP', 1, 0),
+ (11, 'cl45hjt3v0034qnopbthd60ec', 'OWNERSHIP', 1, 0),
+ (12, 'cl463s0780546pdop6iwq014b', 'OWNERSHIP', 1, 0),
+ (13, 'cl45wfa750192ppopl6puaubs', 'OWNERSHIP', 1, 0),
+ (14, 'cl3ralqx800190qop0eb1rctf', 'OWNERSHIP', 1, 0),
+ (15, 'cl4nw2von1305pcop26a39raa', 'OWNERSHIP', 1, 0),
+ (16, 'cl4nw2von1305pcop26a39raa', 'OWNERSHIP', 1, 0),
+ (17, 'cl3yhfeb80346jxopyc94e1v3', 'OWNERSHIP', 1, 0),
+ (18, 'cl3yhfeb80346jxopyc94e1v3', 'OWNERSHIP', 1, 0),
+ (19, 'cl3yhfeb80346jxopyc94e1v3', 'OWNERSHIP', 1, 0),
+ (20, 'cl4xac0073816pcoprkbcr4w1', 'OWNERSHIP', 1, 0),
+ (21, 'cl4xxz5yi4050pcopjc9fa8ts', 'OWNERSHIP', 1, 0),
+ (22, 'cl3ralqx800190qop0eb1rctf', 'OWNERSHIP', 1, 0),
+ (23, 'cl50fnq0c6644pcop87cc8z0t', 'OWNERSHIP', 1, 0),
+ (24, 'cl5100wkw6793pcoptfb4mvof', 'OWNERSHIP', 1, 0),
+ (25, 'cl51ejlys7229pcop9n9e7ijg', 'OWNERSHIP', 1, 0),
+ (26, 'cl4xkdjkk3944pcopqp36ystr', 'OWNERSHIP', 1, 0),
+ (27, 'cl5b05t0z10459pcoptr3m2pvo', 'OWNERSHIP', 1, 0),
+ (28, 'cl5bivaaz0052xuopr19f2ak3', 'OWNERSHIP', 1, 0)
+;
+
+update "Certificate" set "cuid" = 'cl79kcvu1000101ol3u5g1avh' where "id" = 2 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kfeto000001ll8z61goyl' where "id" = 3 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000201ol369z68ow' where "id" = 4 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvtz000001ol0v366vt7' where "id" = 6 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000301ol3dyb6kak' where "id" = 7 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000401ol45fldr1a' where "id" = 8 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000501ol0bf0h563' where "id" = 9 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000601olhudx1d6t' where "id" = 10 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000701olcjtjg217' where "id" = 11 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000801olc10f0dxm' where "id" = 12 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000901ole5xy7krw' where "id" = 13 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000a01ol3vxe3syj' where "id" = 14 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000b01ol44eqbszr' where "id" = 15 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000c01ol6k9273cc' where "id" = 16 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000d01olbkz58w9p' where "id" = 17 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000e01ol0z45fdca' where "id" = 18 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000f01olefle59fx' where "id" = 19 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000g01olebre5tzl' where "id" = 20 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000h01ol9o1x2q3s' where "id" = 21 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000i01olh4adhbpv' where "id" = 22 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000j01olf3rg6lzn' where "id" = 23 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000k01ol92fzb88b' where "id" = 24 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000l01olaxku5l7z' where "id" = 25 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000m01ol4k510c94' where "id" = 26 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000n01olg5vlgcaa' where "id" = 27 and "cuid" is null;
+update "Certificate" set "cuid" = 'cl79kcvu1000o01ol8zp9azcj' where "id" = 28 and "cuid" is null;
