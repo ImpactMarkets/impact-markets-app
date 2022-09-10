81a0b17d3dfc9d148fdb5948ba95680ff6427237
Dawn Drescher <dawn.drescher@impactmarkets.io>
2022-09-10 17:35:21 +0200

    Update copyright year

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react'
 
-import { HeartFilledIcon } from '@/components/icons'
-
-import packageJson from '../package.json'
-
 export function Footer() {
   return (
     <footer className="flex flex-col items-center justify-between gap-2 text-sm md:gap-4 md:flex-row text-secondary">
       <div className="inline-flex justify-center items-center gap-1 text-sm">
-        <span>© Copyright 2022, Good Exchange, PBC All Rights Reserved.</span>
+        <span>
+          © {new Date().getFullYear()}, Good Exchange, PBC. All rights reserved.
+        </span>
       </div>
     </footer>
   )
