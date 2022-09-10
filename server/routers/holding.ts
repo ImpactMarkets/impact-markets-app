ac51bb6734fb4fa04c27006b81b0c6c2c1bce05b
Dawn Drescher <dawn.drescher@impactmarkets.io>
2022-09-10 18:03:20 +0200

    Add holding mutation

--- server/routers/holding.ts
+++ server/routers/holding.ts
@@ -1,29 +1,52 @@
 import { z } from 'zod'
 
+import { Prisma } from '@prisma/client'
+
 import { createProtectedRouter } from '../create-protected-router'
 
-export const holdingRouter = createProtectedRouter().query('feed', {
-  input: z.object({
-    certificateId: z.number(),
-  }),
-  async resolve({ input, ctx }) {
-    const holdings = await ctx.prisma.holding.findMany({
-      orderBy: {
-        size: 'desc',
-      },
-      where: {
-        certificateId: input.certificateId,
-      },
-      select: {
-        id: true,
-        certificateId: true,
-        user: true,
-        type: true,
-        size: true,
-        cost: true,
-      },
-    })
+export const holdingRouter = createProtectedRouter()
+  .query('feed', {
+    input: z.object({
+      certificateId: z.number(),
+    }),
+    async resolve({ input, ctx }) {
+      const holdings = await ctx.prisma.holding.findMany({
+        orderBy: {
+          size: 'desc',
+        },
+        where: {
+          certificateId: input.certificateId,
+        },
+        select: {
+          id: true,
+          certificateId: true,
+          user: true,
+          type: true,
+          size: true,
+          cost: true,
+          valuation: true,
+        },
+      })
 
-    return holdings
-  },
-})
+      return holdings
+    },
+  })
+  .mutation('edit', {
+    input: z.object({
+      id: z.number(),
+      valuation: z.string(),
+    }),
+    async resolve({ input: { id, valuation: valuation_ }, ctx }) {
+      const valuation = new Prisma.Decimal(valuation_)
+      // updateMany only to make the userId condition work – should still always be 0 or 1
+      await ctx.prisma.holding.updateMany({
+        where: {
+          id,
+          userId: ctx.session.user.id,
+        },
+        data: {
+          valuation,
+        },
+      })
+    },
+  })
