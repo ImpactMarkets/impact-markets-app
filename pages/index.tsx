370631aeae66bf9758eb29a4cca0eb1caa00ea71
daniablank <danielblank@berkeley.edu>
2022-10-17 19:32:50 -0400

    Attempt to move intercom settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,13 @@ import { Pagination, getQueryPaginationInput } from '@/components/pagination'
 import { InferQueryPathAndInput, trpc } from '@/lib/trpc'
 import type { NextPageWithAuthAndLayout } from '@/lib/types'
 
+declare global {
+  interface Window {
+    intercomSettings: any
+    Intercom: any
+  }
+}
+
 const CertificateSummary = dynamic<CertificateSummaryProps>(
   () =>
     import('@/components/certificate-summary').then(
@@ -31,6 +38,45 @@ const Home: NextPageWithAuthAndLayout = () => {
     getQueryPaginationInput(ITEMS_PER_PAGE, currentPageNumber),
   ]
   const feedQuery = trpc.useQuery(feedQueryPathAndInput)
+
+  //Attempting to move intercom settings here.
+  window.intercomSettings = {
+    api_base: 'https://api-iam.intercom.io',
+    app_id: 'gbn0p3de',
+  }
+  ;(function () {
+    var w = window
+    var ic = w.Intercom
+    if (typeof ic === 'function') {
+      ic('reattach_activator')
+      ic('update', w.intercomSettings)
+    } else {
+      var d = document
+      var i = function () {
+        i.c(arguments)
+      }
+      i.q = []
+      i.c = function (args) {
+        i.q.push(args)
+      }
+      w.Intercom = i
+      var l = function () {
+        var s = d.createElement('script')
+        s.type = 'text/javascript'
+        s.async = true
+        s.src = 'https://widget.intercom.io/widget/gbn0p3de'
+        var x = d.getElementsByTagName('script')[0]
+        x.parentNode.insertBefore(s, x)
+      }
+      if (document.readyState === 'complete') {
+        l()
+      } else if (w.attachEvent) {
+        w.attachEvent('onload', l)
+      } else {
+        w.addEventListener('load', l, false)
+      }
+    }
+  })()
   const likeMutation = trpc.useMutation(['certificate.like'], {
     onMutate: async (likedCertificateId) => {
       await utils.cancelQuery(feedQueryPathAndInput)
@@ -151,18 +197,6 @@ const Home: NextPageWithAuthAndLayout = () => {
     </div>
   )
 }
-
-<script>
-  window.intercomSettings = {
-    api_base: "https://api-iam.intercom.io",
-    app_id: "gbn0p3de"
-  };
-</script>
-
-<script>
-// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/gbn0p3de'
-(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/gbn0p3de';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-</script>
 
 Home.auth = true
 
