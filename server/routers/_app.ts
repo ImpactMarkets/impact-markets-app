967e02b2db6329788a9afcf519889d412d85e17c
Dawn Drescher <denis.drescher@claviger.net>
2022-10-31 17:16:18 +0100

    Add Decimal support differently

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,11 +1,24 @@
 import superjson from 'superjson'
 
+import { Prisma } from '@prisma/client'
+
 import { createRouter } from '../create-router'
 import { certificateRouter } from './certificate'
 import { commentRouter } from './comment'
 import { holdingRouter } from './holding'
 import { transactionRouter } from './transaction'
 import { userRouter } from './user'
+
+// https://github.com/blitz-js/superjson/blob/12129e1172ed89c69e16c21f00e793618c68d197/README.md#recipes
+superjson.registerCustom<Prisma.Decimal, string>(
+  {
+    isApplicable: (value): value is Prisma.Decimal =>
+      Prisma.Decimal.isDecimal(value),
+    serialize: (value) => value.toJSON(),
+    deserialize: (value) => new Prisma.Decimal(value),
+  },
+  'decimal.js'
+)
 
 export const appRouter = createRouter()
   .transformer(superjson)
