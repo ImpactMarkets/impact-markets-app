585bf6a7f85b75c3505720975cf710ba757cfadc
Dawn Drescher <denis.drescher@claviger.net>
2022-11-01 22:06:07 +0100

    Refactor bonding curve code

--- lib/auction.ts
+++ lib/auction.ts
@@ -12,11 +12,13 @@ export class BondingCurve {
   /* A scale factor k based on the fundraising target (90% sold) */
   scaleFactor = () => this.target.times(3).dividedBy(TARGET_FRACTION ** 3)
 
-  /* The basic shape of the bonding curve: f(x) = kx^2 */
+  /* The basic shape of the bonding curve: f(x) = kx^2.
+     Note that the implied starting valuation here is 0. */
   valuationAt = (fraction: Prisma.Decimal) =>
     this.scaleFactor().times(fraction.toPower(2))
 
-  /* The integral of the bonding curve: F(x) = (1/3)k * x^3 */
+  /* The integral of the bonding curve: F(x) = (1/3)k * x^3.
+     Note again that the implied starting valuation here is 0. */
   costAt = (fraction: Prisma.Decimal) =>
     this.scaleFactor().dividedBy(3).times(fraction.toPower(3))
 
@@ -24,10 +26,27 @@ export class BondingCurve {
   fractionAt = (valuation: Prisma.Decimal) =>
     valuation.dividedBy(this.scaleFactor()).squareRoot()
 
-  /* Definite integral of the bonding curve between a calculated fraction that corresponds to the
-     starting valuation and the sum of that fraction and the given fraction */
-  costBetween = (valuation: Prisma.Decimal, fraction: Prisma.Decimal) =>
-    this.costAt(this.fractionAt(valuation).plus(fraction)).minus(
-      this.costAt(this.fractionAt(valuation))
-    )
+  /* Definite integral of the bonding curve between two fractions */
+  costBetween = (fractionLow: Prisma.Decimal, fractionHigh: Prisma.Decimal) =>
+    this.costAt(fractionHigh).minus(this.costAt(fractionLow))
+
+  /* Convenience function for calculating the maximum valuation from a given size at a given
+     starting valuation (e.g., a size of 0.5 could move the valuation from fraction 0.9 to 1.4 on
+     the bonding curve) */
+  valuationOfSize = (valuation: Prisma.Decimal, size: Prisma.Decimal) => {
+    const fraction = this.fractionAt(valuation)
+    return this.valuationAt(fraction.plus(size))
+  }
+
+  /* Convenience function for calculating the maximum fundraise from a given size at a given
+     starting valuation (e.g., a size of 0.5 could move the valuation from fraction 0.9 to 1.4 on
+     the bonding curve) */
+  costOfSize = (
+    valuation: Prisma.Decimal,
+    size: Prisma.Decimal,
+    offset: Prisma.Decimal = new Prisma.Decimal(0)
+  ) => {
+    const fractionLow = this.fractionAt(valuation).plus(offset)
+    return this.costBetween(fractionLow, fractionLow.plus(size))
+  }
 }
