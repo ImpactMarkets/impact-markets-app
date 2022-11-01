740fdb11cec84d6a2879bada31252d1b41abcd63
Dawn Drescher <denis.drescher@claviger.net>
2022-11-02 00:54:46 +0100

    Create migration for ID change

---
+++ prisma/migrations/20221101232646_change-cert-ids/migration.sql
@@ -0,0 +1,48 @@
+-- AlterTable
+ALTER TABLE "Certificate" DROP CONSTRAINT "Certificate_pkey",
+    ADD COLUMN "oldId" INTEGER,
+    ALTER COLUMN "id" DROP DEFAULT,
+    ALTER COLUMN "id"
+SET DATA TYPE TEXT,
+    ADD CONSTRAINT "Certificate_pkey" PRIMARY KEY ("id");
+
+DROP SEQUENCE "Post_id_seq";
+
+-- AlterTable
+ALTER TABLE "Comment"
+ALTER COLUMN "certificateId"
+SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Holding"
+ALTER COLUMN "certificateId"
+SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "LikedCertificate" DROP CONSTRAINT "LikedCertificate_pkey",
+    ALTER COLUMN "certificateId"
+SET DATA TYPE TEXT,
+    ADD CONSTRAINT "LikedCertificate_pkey" PRIMARY KEY ("certificateId", "userId");
+
+UPDATE "Certificate"
+SET "oldId" = "id"::int;
+
+UPDATE "Comment"
+SET "certificateId" = "Certificate"."cuid"
+FROM "Certificate"
+WHERE "Comment"."certificateId" = "Certificate"."id";
+
+UPDATE "Holding"
+SET "certificateId" = "Certificate"."cuid"
+FROM "Certificate"
+WHERE "Holding"."certificateId" = "Certificate"."id";
+
+UPDATE "LikedCertificate"
+SET "certificateId" = "Certificate"."cuid"
+FROM "Certificate"
+WHERE "LikedCertificate"."certificateId" = "Certificate"."id";
+
+UPDATE "Certificate"
+SET "id" = "cuid";
+
+ALTER TABLE "Certificate" DROP COLUMN "cuid";
