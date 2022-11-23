c0241f61bad5c54af31de1d8b39ab3f873f6c0b7
Lucie Philippon <lucie.philippon@proton.me>
2022-11-23 22:39:06 +0100

    A new look for a new life

--- components/user.tsx
+++ components/user.tsx
@@ -27,7 +27,7 @@ export function User() {
   return (
     <Box className="border-t">
       <Link href={`/profile/${session!.user.id}`}>
-        <a className="w-full block p-2 hover:bg-gray-50">
+        <a className="w-full block p-3 hover:bg-gray-50">
           <div className="flex gap-4 items-center">
             <Avatar
               name={session.user.name}
