9cf399eef5705360df921e191a4baf12e322d82f
Dawn Drescher <denis.drescher@claviger.net>
2022-11-29 22:49:25 +0100

    Remove old sample test

--- tests/e2e/gotopagesample.test.ts
+++ tests/e2e/gotopagesample.test.ts
@@ -1,4 +1,4 @@
-import { type Page, expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 test.beforeEach(async ({ page }) => {
   await page.goto('http://localhost:3000/')
