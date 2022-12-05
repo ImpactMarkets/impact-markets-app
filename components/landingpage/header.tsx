a1ba59728c69e1676a9877f56c53a8c7a5674966
MattB543 <45475210+MattB543@users.noreply.github.com>
2022-12-05 15:12:03 -0800

    Add landing page header

--- components/landingpage/header.tsx
+++ components/landingpage/header.tsx
@@ -1,5 +1,122 @@
+import { signIn } from 'next-auth/react'
+import Link from 'next/link'
 import * as React from 'react'
 
-export function Header() {
-  return <div>Test</div>
+import { Button } from '@/components/button'
+import { Logo } from '@/components/icons'
+import {
+  Burger,
+  Center,
+  Container,
+  Group,
+  Header,
+  Menu,
+  createStyles,
+} from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
+import { IconChevronDown } from '@tabler/icons'
+
+const HEADER_HEIGHT = 60
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    height: HEADER_HEIGHT,
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+
+  links: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  link: {
+    display: 'block',
+    lineHeight: 1,
+    padding: '8px 12px',
+    borderRadius: theme.radius.sm,
+    textDecoration: 'none',
+    color:
+      theme.colorScheme === 'dark'
+        ? theme.colors.dark[0]
+        : theme.colors.gray[7],
+    fontSize: theme.fontSizes.sm,
+    fontWeight: 500,
+
+    '&:hover': {
+      backgroundColor:
+        theme.colorScheme === 'dark'
+          ? theme.colors.dark[6]
+          : theme.colors.gray[0],
+    },
+  },
+
+  linkLabel: {
+    marginRight: 5,
+  },
+}))
+
+export function Landing_Header() {
+  const { classes } = useStyles()
+  const [opened, { toggle }] = useDisclosure(false)
+
+  const links = [
+    {
+      link: '/why',
+      label: 'Why impact markets?',
+      links: null,
+    },
+    {
+      link: 'https://impactmarkets.substack.com/',
+      label: 'Our Blog',
+      links: null,
+    },
+  ]
+  const items = links.map((link) => {
+    return (
+      <div key={link.label} className={classes.link}>
+        <Link href={link.link} onClick={(event) => event.preventDefault()}>
+          {link.label}
+        </Link>
+      </div>
+    )
+  })
+
+  return (
+    <Header height={HEADER_HEIGHT} sx={{ borderBottom: 0 }} mb={120}>
+      <Container className={classes.inner} fluid>
+        <Group>
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+          />
+          <Logo className="w-auto h-[64px]" />
+        </Group>
+        <Group spacing={5} className={classes.links}>
+          {items}
+          <Menu>
+            <div className="inline-flex gap-1 ml-4">
+              <Button onClick={() => signIn()} variant="secondary">
+                Sign up
+              </Button>
+            </div>
+          </Menu>
+        </Group>
+      </Container>
+    </Header>
+  )
 }
+
+/*export function LandingHeader() {
+  return <div>THIS IS A HEADER</div>
+}*/
--- pages/page/landing-page.tsx
+++ pages/page/landing-page.tsx
@@ -1,21 +1,15 @@
 import Head from 'next/head'
 
-import { Heading1 } from '@/components/heading-1'
-import { Header } from '@/components/landingpage/header'
+import { Landing_Header } from '@/components/landingpage/header'
 import type { NextPageWithAuthAndLayout } from '@/lib/types'
 
 const LandingPage: NextPageWithAuthAndLayout = () => {
   return (
-    <div className="max-w-[720px] mx-auto my-5 py-6">
+    <div className="max-w-[1100px] mx-auto my-5 py-6">
       <Head>
         <title>Impact Markets</title>
       </Head>
-
-      <Header></Header>
-
-      <Heading1>Landing page</Heading1>
-
-      <div className="mt-6">Landing page</div>
+      <Landing_Header></Landing_Header>
     </div>
   )
 }
