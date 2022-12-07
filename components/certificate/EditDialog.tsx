517cb3c7310308717dbba100b49920fd572ed146
Dawn Drescher <dawn.drescher@impactmarkets.io>
2022-12-07 01:42:29 +0100

    Set a sensible minimum target

--- components/certificate/EditDialog.tsx
+++ components/certificate/EditDialog.tsx
@@ -12,7 +12,12 @@ import {
 } from '@/components/dialog'
 import { TextField } from '@/components/text-field'
 import { BondingCurve } from '@/lib/auction'
-import { DEFAULT_TARGET, DEFAULT_VALUATION, SHARE_COUNT } from '@/lib/constants'
+import {
+  DEFAULT_TARGET,
+  DEFAULT_VALUATION,
+  SHARE_COUNT,
+  TARGET_FRACTION,
+} from '@/lib/constants'
 import { num } from '@/lib/text'
 import { trpc } from '@/lib/trpc'
 import { Prisma } from '@prisma/client'
@@ -78,6 +83,18 @@ export function EditDialog({
     )
   }
 
+  // TODO: I’m a bit fuzzy on what’s happening here. I would’ve thought that a hypothetical min.
+  // target would be achieved if the bonding curve is close to flat, so with a very small positive
+  // k. So if it’s flat enough, valuation * size * 0.9 shouold approxiate the target. But that
+  // doesn’t make much sense because if it’s flat, it will never reach the valuation in the first
+  // place, and and since flatness is an approximation, it will in fact reach the valuation only
+  // at some very large fraction, where the slope is considerable again and the flatness
+  // approximation is meaningless. This is all very confusing. We also can’t just plug these
+  // numbers into the bonding curve because the slop changes based on the target, so that there’s
+  // a feedback loop. It always seems to tend toward some limit when I manually iterate it, but
+  // that’s about as far as I’ve gotten. I’m not sure what the right approach is here.
+  const minTarget = watchValuation.times(holding.size).times(TARGET_FRACTION)
+
   return (
     <Dialog isOpen={isOpen} onClose={handleClose}>
       <form onSubmit={handleSubmit(onSubmit)}>
@@ -101,12 +118,15 @@ export function EditDialog({
             <TextField
               {...register('target', { required: true })}
               label="Fundraising target"
-              description="How much do you hope to raise?"
+              description={`How much do you hope to raise (min. $${num(
+                minTarget.ceil(),
+                0
+              )})?`}
               rightSection="USD"
               classNames={{ rightSection: 'w-16' }}
               type="number"
               step="0.01"
-              min="1"
+              min={minTarget.ceil().toNumber()}
               max="1e30"
               required
             />
