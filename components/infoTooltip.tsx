bea947246f3cc3fa7c2d733d41394f110a1d9339
Dawn Drescher <dawn.drescher@impactmarkets.io>
2023-01-25 01:16:13 +0000

    Switch to question mark icon

--- components/infoTooltip.tsx
+++ components/infoTooltip.tsx
@@ -1,8 +1,7 @@
 import * as React from 'react'
 
 import { Tooltip } from '@mantine/core'
-
-import { InfoIcon } from './icons'
+import { IconQuestionCircle } from '@tabler/icons'
 
 type InfoTooltipProps = {
   text?: string | React.ReactElement
@@ -12,7 +11,7 @@ export function InfoTooltip({ text }: InfoTooltipProps) {
   return (
     <Tooltip label={text} events={{ hover: true, focus: true, touch: true }}>
       <span>
-        <InfoIcon className="inline h-3 ml-1 align-baseline" />
+        <IconQuestionCircle className="inline h-5 ml-1 align-middle text-gray-400" />
       </span>
     </Tooltip>
   )
