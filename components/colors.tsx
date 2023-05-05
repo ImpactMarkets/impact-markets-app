025e328387425a466f30b9c9278f9d92e308e203
goblin <factotumfrankie@gmail.com>
2023-05-05 14:54:41 -0700

    [#78] new color tags
    added colors.tsx with a color scheme, applied it to project and bounty tags

---
+++ components/colors.tsx
@@ -0,0 +1,13 @@
+interface Colors {
+  [key: string]: string
+}
+
+export const colors: Colors = {
+  work: '#4fcdf7',
+  field: '#47d6ab',
+  bounty: '#4fcdf7',
+  prize: '#47d6ab',
+  other: '#f2b600',
+  community: '#a18Bf5',
+  location: '#fd812d',
+}
