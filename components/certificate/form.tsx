02987e6298964895ac46b4fe375b7c3ee18c90c6
goblin <factotumfrankie@gmail.com>
2023-05-12 11:34:18 -0700

    [#80] update certificate form

--- components/certificate/form.tsx
+++ components/certificate/form.tsx
@@ -135,24 +135,16 @@ export function CertificateForm({
         required
         className="my-6"
       />
-      <SimpleGrid cols={2} breakpoints={[{ maxWidth: 'md', cols: 1 }]}>
-        <TextField
-          {...register('actionStart', { required: true, valueAsDate: true })}
-          label="Start of the work period"
-          description="When did you (or will you) start working on this?"
-          info="You can edit it later."
-          type="date"
-          required
-        />
+      <div className="mt-6">
         <TextField
           {...register('actionEnd', { required: true, valueAsDate: true })}
-          label="End of the work period"
-          description="… finish working on this?"
+          label="Review date"
+          description="When (just estimate) will the outputs of the project be ready for a review? (Optional)"
           info="You can edit it later."
           type="date"
           required
         />
-      </SimpleGrid>
+      </div>
       <div className="mt-6">
         <Controller
           name="content"
