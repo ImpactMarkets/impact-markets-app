c17724984908d66883c8b6e20b529befefeaaa42
Dawn Drescher <denis.drescher@claviger.net>
2023-07-03 15:41:16 +0200

    Fix cert sort

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,3 +12,12 @@ export type ProjectSortKey = (typeof PROJECT_SORT_KEYS)[number]
 
 export const BOUNTY_SORT_KEYS = ['deadline', 'size', 'createdAt', ''] as const
 export type BountySortKey = (typeof BOUNTY_SORT_KEYS)[number]
+
+export const CERTIFICATE_SORT_KEYS = [
+  'supporterCount',
+  'createdAt',
+  'actionStart',
+  'actionEnd',
+  '',
+] as const
+export type CertificateSortKey = (typeof CERTIFICATE_SORT_KEYS)[number]
