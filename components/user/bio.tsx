945b85e76e34aac27f36913e67364c9ea5ea69ed
Dawn Drescher <denis.drescher@claviger.net>
2023-08-10 18:29:38 +0200

    Display default bio

--- components/user/bio.tsx
+++ components/user/bio.tsx
@@ -5,7 +5,9 @@ import { InferQueryOutput } from '@/lib/trpc'
 export function Bio({ user }: { user: InferQueryOutput<'user.profile'> }) {
   return (
     <div className="flow-root mt-6">
-      <div className="border rounded py-10 px-10">{user.bio}</div>
+      <div className="border rounded py-10 px-10">
+        {user.bio || 'This user has not entered a bio yet'}
+      </div>
     </div>
   )
 }
