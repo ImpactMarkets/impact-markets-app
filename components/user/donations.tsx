3aa1ef1f280c55c1f0b22d072e29944a71abf710
Dawn Drescher <denis.drescher@claviger.net>
2023-08-10 18:33:51 +0200

    Tweak donation aggregation and use project data from backend

--- components/user/donations.tsx
+++ components/user/donations.tsx
@@ -1,74 +1,90 @@
+import { flow, max, min } from 'lodash'
+import { map, reverse, sortBy } from 'lodash/fp'
 import Link from 'next/link'
 import * as React from 'react'
 
 import { Date } from '@/components/date'
-import { InferQueryOutput, trpc } from '@/lib/trpc'
+import { num } from '@/lib/text'
+import { InferQueryOutput } from '@/lib/trpc'
+import { Prisma } from '@prisma/client'
+
+interface DonationGroup {
+  projectId: string
+  project: InferQueryOutput<'user.profile'>['donations'][0]['project']
+  totalAmount: Prisma.Decimal
+  earliestDate: Date
+  latestDate: Date
+}
+
+interface GroupedDonations {
+  [projectId: string]: DonationGroup
+}
 
 export function Donations({
   user,
 }: {
   user: InferQueryOutput<'user.profile'>
 }) {
-  const projectQuery = trpc.useQuery(['project.feed'])
-  const project = projectQuery.data?.projects || []
-
-  interface GroupedDonations {
-    [projectId: string]: {
-      projectId: string
-      totalAmount: number
-      earliestDate: Date
-    }
-  }
-
   const groupedDonations = user.donations.reduce((acc, donation) => {
-    // skip donations that are not confirmed or have totalAmount of 0
+    // Skip donations that are not confirmed or have totalAmount of 0
     if (donation.state !== 'CONFIRMED') {
       return acc
     }
 
-    // if the project id is not in the accumulator, add it with the following properties
-    if (!acc[donation.projectId]) {
-      acc[donation.projectId] = {
-        projectId: donation.projectId,
-        totalAmount: 0,
+    // If the project ID is not in the accumulator, add it with the following properties
+    if (!acc[donation.project.id]) {
+      acc[donation.project.id] = {
+        projectId: donation.project.id,
+        project: donation.project,
+        totalAmount: new Prisma.Decimal(0),
         earliestDate: donation.time,
+        latestDate: donation.time,
       }
     }
 
-    // adds donation amount to totalAmount
-    acc[donation.projectId].totalAmount += Number(donation.amount)
+    const aggregate = acc[donation.project.id]
 
-    // replaces date with date of earliest donation
-    if (acc[donation.projectId].earliestDate > donation.time) {
-      acc[donation.projectId].earliestDate = donation.time
-    }
+    // Add donation amount to totalAmount
+    aggregate.totalAmount = acc[donation.project.id].totalAmount.add(
+      donation.amount
+    )
+
+    // Replace date with date of earliest/latest donation
+    aggregate.earliestDate =
+      min([aggregate.earliestDate, donation.time]) || aggregate.earliestDate
+    aggregate.latestDate =
+      max([aggregate.latestDate, donation.time]) || aggregate.latestDate
 
     return acc
   }, {} as GroupedDonations)
 
-  const projectDonations = Object.values(groupedDonations).map((donation) => {
-    const matchingProject = project.find(
-      (project) => project.id === donation.projectId
-    )
-
-    if (!matchingProject) {
-      return null
-    }
-
-    return (
-      <tr className="border" key={donation.projectId}>
-        <td className="p-4">
-          <Link href={`/project/${matchingProject.id}`}>
-            {matchingProject.title}
-          </Link>
+  const projectDonations = flow(
+    Object.values,
+    sortBy('latestDate'),
+    reverse,
+    map((donation: DonationGroup) => (
+      <tr key={donation.project.id}>
+        <td className="px-4 py-2">
+          {donation.project.hidden ? (
+            <span className="text-gray-500" title="Unlisted project">
+              {donation.project.title}
+            </span>
+          ) : (
+            <Link href={`/project/${donation.project.id}`} className="link">
+              {donation.project.title}
+            </Link>
+          )}
         </td>
-        <td className="p-4">${donation.totalAmount}</td>
-        <td className="p-4">
+        <td className="px-4 py-2">
           <Date date={donation.earliestDate} />
         </td>
+        <td className="px-4 py-2">
+          <Date date={donation.latestDate} />
+        </td>
+        <td className="px-4 py-2 text-right">${num(donation.totalAmount)}</td>
       </tr>
-    )
-  })
+    ))
+  )(groupedDonations)
 
   return (
     <table className="table-auto w-full">
