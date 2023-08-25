e6d8239fcebd57a35280bf4cac3450da40a30191
Dawn Drescher <dawn.drescher@impactmarkets.io>
2023-08-25 15:22:38 +0200

    Continue tRPC upgrade

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,16 +7,11 @@ import * as React from 'react'
 
 import { browserEnv } from '@/env/browser'
 import { emotionCache } from '@/lib/emotionCache'
-import { transformer } from '@/lib/trpc'
+import { trpc } from '@/lib/trpc'
 import type { NextPageWithAuthAndLayout } from '@/lib/types'
-import { AppRouter } from '@/server/routers/_app'
 import { MantineProvider } from '@mantine/core'
 import { Provider as RollbarProvider } from '@rollbar/react'
 import TawkMessengerReact from '@tawk.to/tawk-messenger-react'
-import { httpBatchLink } from '@trpc/client/links/httpBatchLink'
-import { loggerLink } from '@trpc/client/links/loggerLink'
-import { withTRPC } from '@trpc/next'
-import { TRPCError } from '@trpc/server'
 
 import '../styles/globals.css'
 
@@ -25,7 +20,7 @@ const Toaster = dynamic(
   () => import('react-hot-toast').then((c) => c.Toaster),
   {
     ssr: false,
-  }
+  },
 )
 
 const barlowCondensed = Barlow_Condensed({
@@ -125,58 +120,3 @@ function MyApp({
 }
 
 export default trpc.withTRPC(MyApp)
-
-  if (isUser) {
-    return <>{children}</>
-  }
-
-  // Session is being fetched, or no user.
-  // If no user, useEffect() will redirect.
-  return null
-}
-
-function getBaseUrl() {
-  if (process.browser) {
-    return ''
-  }
-
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`
-  }
-
-  return `http://localhost:${process.env.PORT ?? 3000}`
-}
-
-export default withTRPC<AppRouter>({
-  config() {
-    return {
-      links: [
-        loggerLink({
-          enabled: (opts) =>
-            process.env.NODE_ENV === 'development' ||
-            (opts.direction === 'down' && opts.result instanceof Error),
-        }),
-        httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
-        }),
-      ],
-      transformer,
-      queryClientConfig: {
-        defaultOptions: {
-          queries: {
-            retry: (failureCount, error: any) => {
-              const trcpErrorCode = error?.data?.code as TRPCError['code']
-              if (trcpErrorCode === 'NOT_FOUND') {
-                return false
-              }
-              if (failureCount < 3) {
-                return true
-              }
-              return false
-            },
-          },
-        },
-      },
-    }
-  },
-})(MyApp)
