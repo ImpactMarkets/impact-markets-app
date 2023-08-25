a44843c63eae5e99a113f308ece1a623f0555cd8
Dawn Drescher <dawn.drescher@impactmarkets.io>
2023-08-25 15:51:24 +0200

    Simplify design

--- components/welcome/heroText.tsx
+++ components/welcome/heroText.tsx
@@ -3,80 +3,14 @@ import { Container, Text, Title, createStyles } from '@mantine/core'
 import { buttonClasses } from '../button'
 import { ButtonLink } from '../buttonLink'
 
-const useStyles = createStyles((theme) => ({
-  wrapper: {
-    position: 'relative',
-    paddingTop: 0,
-    paddingBottom: 80,
-
-    '@media (max-width: 755px)': {
-      paddingTop: 80,
-      paddingBottom: 60,
-    },
-  },
-
-  inner: {
-    position: 'relative',
-    zIndex: 1,
-  },
-
-  dots: {
-    position: 'absolute',
-    color:
-      theme.colorScheme === 'dark'
-        ? theme.colors.dark[5]
-        : theme.colors.gray[1],
-
-    '@media (max-width: 755px)': {
-      display: 'none',
-    },
-  },
-
-  dotsLeft: {
-    left: 0,
-    top: 0,
-  },
-
-  title: {
-    textAlign: 'center',
-    fontWeight: 800,
-    fontSize: 40,
-    letterSpacing: -1,
-    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-    marginBottom: theme.spacing.xs,
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-
-    '@media (max-width: 520px)': {
-      fontSize: 28,
-    },
-  },
-
-  highlight: {
-    color:
-      theme.colors[theme.primaryColor][theme.colorScheme === 'dark' ? 4 : 6],
-  },
-
-  description: {
-    textAlign: 'center',
-
-    '@media (max-width: 520px)': {
-      fontSize: theme.fontSizes.md,
-    },
-  },
-}))
-
 export function HeroText() {
-  const { classes } = useStyles()
-
   return (
     <Container className="max-w-[1400px] pb-24 text-center">
-      <div className={classes.inner}>
-        <Title className={classes.title}>
+      <div>
+        <Title className="text-[40px] mb-2">
           The AI Safety
           <div>
-            <Text component="span" className={classes.highlight} inherit>
-              Charity Evaluator
-            </Text>
+            <span className="text-[#0e73cc]">Charity Evaluator</span>
           </div>
         </Title>
 
