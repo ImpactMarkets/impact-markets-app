eac37a59e5623d6f437d29ded3135e2db8127d40
Dawn Drescher <denis.drescher@claviger.net>
2023-10-19 09:45:39 +0200

    Create the default user

---
+++ prisma/migrations/20231019072135_default_user/migration.sql
@@ -0,0 +1,24 @@
+-- Create the Impact Markets user because some of our code assumes it exists
+INSERT INTO
+    public."User"
+VALUES
+    (
+        'cl9t5obrn000aop8kpp9zxxjc',
+        'Impact Markets',
+        'impactmarkets.io@gmail.com',
+        NULL,
+        'https://lh3.googleusercontent.com/a/ALm5wu1mzmWnVWP95EAGnA7ER3ldW52rGRqOX6_5hgZRxA=s96-c',
+        'We run this platform',
+        'USER',
+        true,
+        'https://paypal.me/impactmarkets',
+        'https://impactmarkets.io/',
+        false,
+        true,
+        '2022-10-27 23:58:59.279',
+        '2000-01-01 00:00:00',
+        '',
+        '',
+        true,
+        true
+    );
