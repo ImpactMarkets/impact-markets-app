15c37e4b8ae1771f1a9239256c44cf3b3ac9f637
Dawn Drescher <denis.drescher@claviger.net>
2023-10-26 21:21:20 +0200

    Add tooltips

--- components/welcome/partners.tsx
+++ components/welcome/partners.tsx
@@ -1,45 +1,60 @@
 // map a partners object to a list of logos',
+import Image from 'next/image'
+
+import { Tooltip } from '@/lib/mantine'
 
 const partners = [
   {
-    name: 'EAGxBerlin',
-    logo: '/images/partners/eagx-berlin.svg',
-  },
-  {
-    name: 'EAGxRotterdam',
-    logo: '/images/partners/eagx-rotterdam.svg',
-  },
-  {
-    name: 'EAGxWarsaw',
-    logo: '/images/partners/eagx-warsaw.svg',
-  },
-  {
-    name: 'Funding the Commons',
-    logo: '/images/partners/funding-the-commons.svg',
-  },
-  {
-    name: 'LessWrong Community Weekend',
-    logo: '/images/partners/less-wrong-community-weekend.svg',
-  },
-  {
-    name: 'VAISU',
-    logo: '/images/partners/vaisu.svg',
-  },
-  {
-    name: 'Glo Consortium',
+    tooltip: 'Member of the Glo Consortium',
     logo: '/images/partners/glo-consortium.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at EAGxBerlin',
+    logo: '/images/partners/eagx-berlin.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at EAGxRotterdam',
+    logo: '/images/partners/eagx-rotterdam.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at EAGxWarsaw',
+    logo: '/images/partners/eagx-warsaw.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at Funding the Commons',
+    logo: '/images/partners/funding-the-commons.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at LessWrong Community Weekend',
+    logo: '/images/partners/less-wrong-community-weekend.svg',
+    aspect: 0.55,
+  },
+  {
+    tooltip: 'Seen at VAISU',
+    logo: '/images/partners/vaisu.svg',
+    aspect: 0.55,
   },
 ]
 
 export const Partners = () => (
-  <div className="max-w-[1400px] text-center bg-gray-100">
-    {partners.map((partner, index) => (
-      <div key={index} className="inline-block w-[200px] h-[110px] m-4">
-        <img
-          src={partner.logo}
-          alt={partner.name}
-          className="w-full h-full object-contain opacity-50"
-        />
+  <div className="flex gap-4 flex-row p-3 w-full bg-gray-100 rounded-2xl">
+    {partners.map(({ logo, tooltip, aspect }, index) => (
+      <div key={index} className="w-[200px]">
+        <Tooltip label={tooltip}>
+          <Image
+            src={logo}
+            alt={tooltip}
+            width={200}
+            height={200 * aspect}
+            className="w-full h-full object-contain opacity-50"
+            unoptimized
+          />
+        </Tooltip>
       </div>
     ))}
   </div>
