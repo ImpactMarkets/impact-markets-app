78e3ce2c785cc7011da00d160fa42e98893679d4
Dawn Drescher <denis.drescher@claviger.net>
2023-11-02 16:44:33 +0100

    Experiment with more algorithm tweaks

--- prisma/migrations/20231030101047_improve-contribution-math/migration.sql
+++ prisma/migrations/20231030101047_improve-contribution-math/migration.sql
@@ -1,5 +1,10 @@
-CREATE
-OR REPLACE VIEW "Contribution" AS (
+DROP VIEW "SupportScore";
+
+DROP VIEW "UserScore";
+
+DROP VIEW "Contribution";
+
+CREATE VIEW "Contribution" AS (
     WITH funding AS (
         SELECT
             -- Choose magic number 100 (or anything else positive) and consider shifted hyperbolic
@@ -12,20 +17,22 @@ OR REPLACE VIEW "Contribution" AS (
     ),
     "contributionsStep1" AS (
         SELECT
-            "userId",
-            "projectId",
-            "time",
-            "amount",
+            "Donation"."userId",
+            "Donation"."projectId",
+            "Donation"."time",
+            "Donation"."amount",
             (
-                "funding"."initial" + SUM("amount") OVER (
-                    PARTITION BY "projectId"
+                "funding"."initial" + SUM("Donation"."amount") OVER (
+                    PARTITION BY "Donation"."projectId"
                     ORDER BY
-                        "time" ASC,
-                        "id" ASC
+                        "Donation"."time" ASC,
+                        "Donation"."id" ASC
                 )
             ) AS "runningTotal",
             (
-                "funding"."initial" + (SUM("amount") OVER (PARTITION BY "projectId"))
+                "funding"."initial" + (
+                    SUM("Donation"."amount") OVER (PARTITION BY "Donation"."projectId")
+                )
             ) AS "projectTotal",
             -- Age of the project in seconds, according to the first donation
             -- Funding goal as product of the age and the per-second funding goal
@@ -33,17 +40,22 @@ OR REPLACE VIEW "Contribution" AS (
                 EPOCH
                 from
                     AGE(
-                        MIN("time") OVER (PARTITION BY "projectId")
+                        LEAST(
+                            MIN("time") OVER (PARTITION BY "Donation"."projectId"),
+                            MIN("Project"."createdAt") OVER (PARTITION BY "Donation"."projectId"),
+                            MIN("Project"."actionStart") OVER (PARTITION BY "Donation"."projectId")
+                        )
                     )
             ) * "funding"."goal" AS "fundingGoal"
         FROM
-            "Donation",
-            funding
+            "funding",
+            "Donation"
+            JOIN "Project" ON "Project"."id" = "Donation"."projectId"
         WHERE
-            "state" = 'CONFIRMED'
+            "Donation"."state" = 'CONFIRMED'
         ORDER BY
-            "projectId" ASC,
-            "time" ASC
+            "Donation"."projectId" ASC,
+            "Donation"."time" ASC
     ),
     "contributionsStep2" AS (
         SELECT
@@ -65,7 +77,7 @@ OR REPLACE VIEW "Contribution" AS (
             *,
             SUM("contribution" :: float) OVER (PARTITION BY "projectId") AS "bareContributionTotal",
             CASE
-                -- Adjustment for crowdedness
+                -- Adjustment for very early-stage projects that a donor can monopolize cheaply
                 WHEN "projectTotal" < "fundingGoal" THEN ln("fundingGoal") / ln("projectTotal")
                 ELSE 0.0
             END + (
@@ -81,7 +93,9 @@ OR REPLACE VIEW "Contribution" AS (
         -- The contributions do not sum to one. We compute their sum so that we can divide by it to
         -- get what fraction of the impact credits each donor should receive. This step can be 
         -- simplified by noting that sum_of_contributions = log(total of contributions + 100)
-        SUM("contribution" / "contributionTotal") :: numeric AS "relativeContribution"
+        SUM("contribution" / "contributionTotal") :: numeric AS "relativeContribution",
+        SUM("contribution" / "bareContributionTotal") :: numeric AS "relativeBareContribution",
+        MAX(SUM("contribution" / "bareContributionTotal")) OVER (PARTITION BY "projectId") AS "maxContribution"
     FROM
         "contributions"
     GROUP BY
@@ -90,4 +104,66 @@ OR REPLACE VIEW "Contribution" AS (
     ORDER BY
         "projectId",
         "relativeContribution" DESC
+);
+
+CREATE VIEW "UserScore" AS (
+    SELECT
+        "userId",
+        SUM("relativeContribution" * "Project"."credits") :: numeric as "score",
+        SUM(
+            CASE
+                WHEN "Project"."updatedAt" > NOW() - INTERVAL '365 days' --
+                THEN "relativeContribution" * "Project"."credits"
+                ELSE 0
+            END
+        ) :: numeric as "score365",
+        RANK() OVER (
+            ORDER BY
+                SUM("relativeContribution" * "Project"."credits") ASC
+        ) as "inverseRank"
+    FROM
+        "Contribution"
+        JOIN "Project" ON "Project"."id" = "Contribution"."projectId"
+    WHERE
+        -- Skip if a project is carried by just one person as that usually means that our
+        -- data is highly incomplete
+        "maxContribution" < 0.99
+    GROUP BY
+        "userId"
+);
+
+CREATE VIEW "SupportScore" AS (
+    WITH "projectsWithNulls" AS (
+        SELECT
+            "Project"."id" as "projectId",
+            COUNT("User"."id") as "count",
+            MAX("Contribution"."maxContribution") as "maxContribution",
+            SUM("relativeContribution" * "UserScore"."score") as "score"
+        FROM
+            "Project"
+            LEFT JOIN "Contribution" ON (
+                -- Skip if a project is carried by just one person as that usually means that our
+                -- data is highly incomplete
+                "Contribution"."projectId" = "Project"."id"
+                AND "Contribution"."maxContribution" < 0.99
+            )
+            LEFT JOIN "UserScore" ON "UserScore"."userId" = "Contribution"."userId"
+            LEFT JOIN "User" ON "User"."id" = "Contribution"."userId"
+        GROUP BY
+            "Project"."id"
+    )
+    SELECT
+        "projectId",
+        "count" :: numeric,
+        "maxContribution" :: numeric,
+        CASE
+            -- Return 0 for incomplete rows because we can’t sort nulls to the end atm.
+            -- https://github.com/prisma/prisma/issues/19761
+            WHEN "score" is not null THEN "score"
+            ELSE 0
+        END :: numeric as "score"
+    FROM
+        "projectsWithNulls"
+    ORDER BY
+        "score" desc
 );
